7a1322250d39a6fac614a2e98d60fbcb3fd118e9
Yassinrby <yassinrby@outlook.com>
2021-01-13 17:27:22 +0000

    Sign in page and function complete

--- app.py
+++ app.py
@@ -40,14 +40,43 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-        return redirect(url_for("register", username=session["user"]))
+        return redirect(url_for("home", username=session["user"]))
 
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "home", username=session["user"]))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 @app.route("/")
-@app.route("/home_page/")
-def home_page():
+@app.route("/home/")
+def home():
     recipes = mongo.db.recipe.find()
     return render_template("home.html", recipes=recipes)
 
